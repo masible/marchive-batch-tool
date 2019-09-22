6786571c7c1c41108b99303ef3b8d7c7064cb48b
Yukai Li <yukaili.geek@gmail.com>
2019-09-22 00:32:04 -0600

    Add initial library of code

---
+++ MArchiveBatchTool/IMArchiveCodec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MArchiveBatchTool
+{
+    public interface IMArchiveCodec
+    {
+        uint Magic { get; }
+        Stream GetDecompressionStream(Stream inStream);
+        Stream GetCompressionStream(Stream inStream);
+    }
+}
