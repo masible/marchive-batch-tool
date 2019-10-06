cd38afff469b390e1787edf937563a3c1e7ed9a6
Yukai Li <yukaili.geek@gmail.com>
2019-10-05 23:39:40 -0600

    Add .bin extractor

---
+++ MArchiveBatchTool/MArchive/AllDataPacker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using MArchiveBatchTool.Psb;
+using MArchiveBatchTool.Models;
+
+namespace MArchiveBatchTool.MArchive
+{
+    class AllDataPacker
+    {
+        public static void UnpackFiles(string psbPath, string outputPath,
+            IMArchiveCodec codec = null, string seed = null, int keyLength = 0)
+        {
+            // Figure out what file we've been given
+            if (Path.GetExtension(psbPath).ToLower() == ".bin")
+                psbPath = Path.ChangeExtension(psbPath, ".psb");
+
+            // No PSB, try .psb.m
+            if (!File.Exists(psbPath)) psbPath += ".m";
+
+            if (Path.GetExtension(psbPath).ToLower() == ".m")
+            {
+                // Decompress .m file
+                MArchivePacker map = new MArchivePacker(codec, seed, keyLength);
+                map.DecompressFile(psbPath, true);
+                psbPath = Path.ChangeExtension(psbPath, null);
+            }
+
+            ArchiveV1 arch;
+            using (FileStream fs = File.OpenRead(psbPath))
+            using (PsbReader reader = new PsbReader(fs))
+            {
+                var root = reader.Root;
+                arch = root.ToObject<ArchiveV1>();
+            }
+
+            if (arch.ObjectType != "archive" || arch.Version != 1.0)
+                throw new InvalidDataException("PSB file is not an archive.");
+
+            using (FileStream fs = File.OpenRead(Path.ChangeExtension(psbPath, ".bin")))
+            {
+                foreach (var file in arch.FileInfo)
+                {
+                    Console.WriteLine($"Extracting {file.Key}");
+                    string outPath = Path.Combine(outputPath, file.Key);
+                    Directory.CreateDirectory(Path.GetDirectoryName(outPath));
+                    using (FileStream ofs = File.Create(outPath))
+                    {
+                        fs.Seek(file.Value[0], SeekOrigin.Begin);
+                        Utils.CopyStream(fs, ofs, file.Value[1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MArchiveBatchTool/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MArchiveBatchTool
+{
+    public static class Utils
+    {
+        // Modified from https://stackoverflow.com/a/230141/1180879
+        public static void CopyStream(Stream input, Stream output, int count)
+        {
+            byte[] buffer = new byte[81920];
+            int read;
+            while (count > 0 && (read = input.Read(buffer, 0, Math.Min(buffer.Length, count))) > 0)
+            {
+                output.Write(buffer, 0, read);
+                count -= read;
+            }
+        }
+    }
+}
